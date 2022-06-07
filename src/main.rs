b74a773713d15abb073f688536b4ffa6c9ab6d61
someoneonsmile <someoneonsmile@gmail.com>
2022-06-08 03:30:00 +0800

    remove old compared comment code

--- src/main.rs
+++ src/main.rs
@@ -68,27 +68,6 @@ where
     P: AsRef<Path>,
     Fut: std::future::Future<Output = Result<()>> + Send + 'static,
 {
-    // let mut handles = Vec::<JoinHandle<Result<()>>>::new();
-    // for pack in packs {
-    //     let pack_config = Config::from_path(pack.join(CONFIG_FILE_NAME))?;
-    //     if pack_config.is_none() {
-    //         warn!(
-    //             "{:?} is not the pack_home (witch contains .stowrc config file)",
-    //             pack
-    //         );
-    //         continue;
-    //     }
-    //     let config = pack_config.merge(common_config.clone()).unwrap();
-    //     handles.push(tokio::spawn(async move {
-    //         reload(&config, pack).await?;
-    //         Ok(())
-    //     }));
-    // }
-    // // TODO: replace with await all, and handle the result
-    // for handle in handles {
-    //     handle.await??;
-    // }
-
     futures::stream::iter(packs.into_iter().map(Ok))
         .try_filter_map(|pack| async {
             let pack_config = Config::from_path(pack.as_ref().join(CONFIG_FILE_NAME))?;
@@ -150,31 +129,6 @@ async fn install<P: AsRef<Path>>(config: Arc<Config>, pack: P) -> Result<()> {
         .await??
     };
 
-    // let mut handles = Vec::<JoinHandle<Result<()>>>::new();
-    // for path in paths {
-    //     let path_target = PathBuf::from(&target).join(path.strip_prefix(pack.deref())?);
-    //     if path_target.exists() {
-    //         if let Some(false) | None = &config.force {
-    //             if let Ok(false) | Err(_) = same_file::is_same_file(&path, &path_target) {
-    //                 error!("target has exists, target:{:?}", path_target);
-    //             }
-    //             continue;
-    //         }
-    //     }
-    //     handles.push(tokio::task::spawn_blocking(move || {
-    //         if let Some(parent) = path_target.parent() {
-    //             fs::create_dir_all(parent)?;
-    //         }
-    //         let _ = fs::remove_file(&path_target);
-    //         info!("install {:?} -> {:?}", path_target, path);
-    //         symlink(&path, &path_target)?;
-    //         Ok(())
-    //     }));
-    // }
-    // for handle in handles {
-    //     handle.await??;
-    // }
-
     futures::stream::iter(paths.into_iter().map(Ok))
         .try_filter_map(|path| async {
             let path_target = PathBuf::from(target).join(path.strip_prefix(pack.deref())?);
@@ -226,14 +180,6 @@ async fn remove<P: AsRef<Path>>(config: Arc<Config>, pack: P) -> Result<()> {
         None => None,
     };
 
-    // let mut paths = Vec::new();
-    // for entry in fs::read_dir(&pack)? {
-    //     let (_, sub_path_option) = CollectBot::new(&entry?.path(), &ignore_re).collect()?;
-    //     if let Some(mut sub_paths) = sub_path_option {
-    //         paths.append(&mut sub_paths);
-    //     }
-    // }
-
     let paths = {
         let pack = pack.clone();
         tokio::task::spawn_blocking(move || {
@@ -248,31 +194,6 @@ async fn remove<P: AsRef<Path>>(config: Arc<Config>, pack: P) -> Result<()> {
         })
         .await??
     };
-
-    // let mut handles = Vec::<JoinHandle<Result<()>>>::new();
-    // for path in paths {
-    //     let path_target =
-    //         PathBuf::from(&target).join(PathBuf::from(&path).strip_prefix(pack.deref())?);
-    //     if !path_target.exists() {
-    //         continue;
-    //     }
-    //     if matches!(
-    //         same_file::is_same_file(&path_target, &path),
-    //         Ok(false) | Err(_)
-    //     ) {
-    //         error!("remove symlink, not same_file, target:{:?}", path_target);
-    //         continue;
-    //     }
-    //     handles.push(tokio::task::spawn_blocking(move || {
-    //         info!("remove {:?} -> {:?}", path_target, path);
-    //         fs::remove_file(&path_target)?;
-    //         Ok(())
-    //     }))
-    // }
-    // // TODO: replace with await all, and handle the result
-    // for handle in handles {
-    //     handle.await??;
-    // }
 
     futures::stream::iter(paths.into_iter().map(Ok))
         .try_filter_map(|path| async {
