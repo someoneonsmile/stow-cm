59d5e792f3b605f4d2d7de6eead84ae3ca0c3677
someoneonsmile <someoneonsmile@gmail.com>
2023-08-13 13:04:18 +0800

    fix: use const instandof static

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,3 @@
-pub(crate) static PACK_NAME_ENV: &str = "PACK_NAME";
+pub(crate) const PACK_NAME_ENV: &str = "PACK_NAME";
 
-pub(crate) static CONFIG_FILE_NAME: &str = "stow-cm.toml";
+pub(crate) const CONFIG_FILE_NAME: &str = "stow-cm.toml";
